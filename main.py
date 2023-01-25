c98f2e5dca0b8b7c3b74b67b061e8a3cf6613b39
greendolf <ghost_4452@bk.ru>
2023-01-26 02:49:06 +0700

    upgraded main

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 from view import MainWindow
-import model as m
 from tkinter import *
-import time
-from threading import Thread, main_thread
 
 
-spis = m.get_spis()
+def main():
 
-root = Tk()
-root.title('Password manager')
-w = root.winfo_screenwidth()
-h = root.winfo_screenheight()
-w = w//2 # середина экрана
-h = h//2 
-w = w - 400 # смещение от середины
-h = h - 350
-root.geometry('800x600+{}+{}'.format(w, h))
-root.resizable(False, False)
-root.iconphoto(True, PhotoImage(file='img\icon.png'))
+    root = Tk()
+    root.title('Password manager')
 
-main = MainWindow(root)
+    w = root.winfo_screenwidth()
+    h = root.winfo_screenheight()
+    w = w//2 # середина экрана
+    h = h//2 
+    w = w - 400 # смещение от середины
+    h = h - 350
 
-def finish():
-    root.destroy()
-root.protocol("WM_DELETE_WINDOW", finish)
+    root.geometry('800x600+{}+{}'.format(w, h))
+    root.resizable(False, False)
+    root.iconphoto(True, PhotoImage(file='img\icon.png'))
 
+    main = MainWindow(root)
 
-root.mainloop()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
